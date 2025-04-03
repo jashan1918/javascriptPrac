75a4fb43c5ce4e2c37fa27d9eaf6a442d12da708
Jashan <jashandeep4329@gmail.com>
2025-04-03 15:57:35 +0530

    datatypes summary

---
+++ 01_basics/datatypesSummary.js
@@ -0,0 +1,6 @@
+//primitive datatypes are call by value which means you dont get the real value you just get a copy
+
+//7 types total string numver boolean null undefined synbol bigInt
+
+
+//Non primitive datatypes also known as the reference datatypes which are obviously called by reference means you can change the real value of them 
