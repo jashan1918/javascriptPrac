035c93ee8cf0280074e732a205257a81ad4a9433
Jashan <jashandeep4329@gmail.com>
2025-04-03 14:46:41 +0530

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 1234687;
+let accountEmail = "jashan@gmail.com";
+var accountPassword = "12345";
+accountCity = "Jaipur"; 
+
+//accountId = 2;
+accountEmail = "skdfjklsajdf"
+accountPassword = "398257"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
