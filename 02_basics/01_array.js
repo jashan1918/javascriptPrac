09ed046e0fe8e5699d1cf2c74cc528ccacc21e28
Jashan <jashandeep4329@gmail.com>
2025-04-04 14:18:44 +0530

    date and time

---
+++ 02_basics/01_array.js
@@ -0,0 +1,6 @@
+//array
+
+const myArr = [1, 2, 3, , 4, 5];
+
+
+console.log(myArr[0]);
---
+++ 02_basics/02_dateandTime.js
@@ -0,0 +1,13 @@
+let count = 10;
+
+function countDown(){
+
+    for(i = count; i >= 0; i--){
+
+        console.log(i)
+    }
+
+    console.log("the time is up!!");
+}
+
+countDown();
