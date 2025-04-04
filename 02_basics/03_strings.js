b93d2ed6c86ead7a53bdf6659152788ad3c356f9
Jashan <jashandeep4329@gmail.com>
2025-04-04 15:17:25 +0530

    string methods prac

---
+++ 02_basics/03_strings.js
@@ -0,0 +1,22 @@
+const str = "hellohello";
+
+console.log(str.length);
+
+console.log(str.slice(0,5));
+console.log(str.slice(5));
+
+const string = "today is a very warm day";
+
+console.log(string.includes("very"));
+console.log(string.includes("hot"));
+
+console.log(string.indexOf("a"));
+
+console.log(string.replace("warm", "hot"));
+
+console.log(string.trim());
+
+console.log(string.split());
+
+const testStr = "javascript";
+
