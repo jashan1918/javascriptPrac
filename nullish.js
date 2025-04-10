8ea35c061ca6519fe24d36a6c3b4197a63f58388
Jashan <jashandeep4329@gmail.com>
2025-04-10 10:45:32 +0530

    did some more basics

---
+++ nullish.js
@@ -0,0 +1,9 @@
+//Nullish coalesing operator (??): null undefined
+
+//when the value is undefined or null it assings the second value to the variable
+
+let val1;
+
+//val1 = null ?? 10;
+val1 = undefined ?? 15;
+
